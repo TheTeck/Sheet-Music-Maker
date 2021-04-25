db22ec659511d147c1ba7fe2977d61a036a2fc37
John Teckemeyer <johnteckemeyer@gmail.com>
2021-04-25 15:14:49 -0700

    Add file counter for Opus Index Page

--- src/pages/OpusIndexPage/OpusIndexPage.jsx
+++ src/pages/OpusIndexPage/OpusIndexPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageHeader from '../../components/Header/Header';
 import UserNav from '../../components/UserNav/UserNav';
 import FileOptionControls from '../../components/FileOptionControls/FileOptionControls';
+import { Grid, Header } from 'semantic-ui-react';
 import './OpusIndexPage.css';
 
 export default function OpusIndexPage({ user, handleLogout }) {
@@ -10,7 +11,18 @@ export default function OpusIndexPage({ user, handleLogout }) {
             <PageHeader user={user} handleLogout={handleLogout} />
             <div className="body">
                 <UserNav user={user} />
-                <FileOptionControls />
+                <Grid columns={2}>
+                    <Grid.Column>
+                        <FileOptionControls />
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Header style={{ padding: '10px' }} floated='right' as='h3'>
+                            You have {user.opera.length} 
+                            {user.opera.length === 1 ? ' file': ' files'}
+                        </Header>
+                    </Grid.Column>
+                </Grid>
+                
                 All of the user's works shall be shown here
             </div>
         </div>
