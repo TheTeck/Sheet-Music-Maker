6e9f44172ce88393a17c0ee08c02e80b8af5a263
John Teckemeyer <johnteckemeyer@gmail.com>
2021-04-29 12:16:12 -0700

    Add all standard flat keysignature options

--- src/components/KeySignature/KeySignature.jsx
+++ src/components/KeySignature/KeySignature.jsx
@@ -65,6 +65,69 @@ export default function KeySignature ({ keySig }) {
                     <div className="sharp-6">♯</div>
                     <div className="sharp-7">♯</div>
                 </div>
+    } else if ( (keySig[0] === 'F' && keySig[1] === 'natural' && keySig[2] === 'major') ||
+        (keySig[0] === 'D' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                </div>
+    } else if ( (keySig[0] === 'B' && keySig[1] === 'flat' && keySig[2] === 'major') ||
+        (keySig[0] === 'G' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                    <div className="flat-2">♭</div>
+                </div>
+    } else if ( (keySig[0] === 'E' && keySig[1] === 'flat' && keySig[2] === 'major') ||
+        (keySig[0] === 'C' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                    <div className="flat-2">♭</div>
+                    <div className="flat-3">♭</div>
+                </div>
+    } else if ( (keySig[0] === 'A' && keySig[1] === 'flat' && keySig[2] === 'major') ||
+        (keySig[0] === 'F' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                    <div className="flat-2">♭</div>
+                    <div className="flat-3">♭</div>
+                    <div className="flat-4">♭</div>
+                </div>
+    } else if ( (keySig[0] === 'D' && keySig[1] === 'flat' && keySig[2] === 'major') ||
+        (keySig[0] === 'B' && keySig[1] === 'flat' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                    <div className="flat-2">♭</div>
+                    <div className="flat-3">♭</div>
+                    <div className="flat-4">♭</div>
+                    <div className="flat-5">♭</div>
+                </div>
+    } else if ( (keySig[0] === 'G' && keySig[1] === 'flat' && keySig[2] === 'major') ||
+        (keySig[0] === 'E' && keySig[1] === 'flat' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                    <div className="flat-2">♭</div>
+                    <div className="flat-3">♭</div>
+                    <div className="flat-4">♭</div>
+                    <div className="flat-5">♭</div>
+                    <div className="flat-6">♭</div>
+                </div>
+    } else if ( (keySig[0] === 'C' && keySig[1] === 'flat' && keySig[2] === 'major') ||
+        (keySig[0] === 'A' && keySig[1] === 'flat' && keySig[2] === 'minor')) {
+            console.log('okay')
+        output = <div className="signature">
+                    <div className="flat-1">♭</div>
+                    <div className="flat-2">♭</div>
+                    <div className="flat-3">♭</div>
+                    <div className="flat-4">♭</div>
+                    <div className="flat-5">♭</div>
+                    <div className="flat-6">♭</div>
+                    <div className="flat-7">♭</div>
+                </div>
     }
 
     return (
