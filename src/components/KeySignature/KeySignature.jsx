84565f078681aa953212fc4f48b1b2c2547c2a10
John Teckemeyer <johnteckemeyer@gmail.com>
2021-04-29 11:49:57 -0700

    Implement display of all sharp key signature

--- src/components/KeySignature/KeySignature.jsx
+++ src/components/KeySignature/KeySignature.jsx
@@ -5,17 +5,71 @@ export default function KeySignature ({ keySig }) {
 
     let output;
 
+    // Standard Keys with sharps
     if ( (keySig[0] === 'C' && keySig[1] === 'natural' && keySig[2] === 'major') ||
         (keySig[0] === 'A' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
         output = <></>
     } else if ( (keySig[0] === 'G' && keySig[1] === 'natural' && keySig[2] === 'major') ||
         (keySig[0] === 'E' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
-        output = <div className="sharp-1">♯</div>
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                </div>
+    } else if ( (keySig[0] === 'D' && keySig[1] === 'natural' && keySig[2] === 'major') ||
+        (keySig[0] === 'B' && keySig[1] === 'natural' && keySig[2] === 'minor')) {
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                    <div className="sharp-2">♯</div>
+                </div>
+    } else if ( (keySig[0] === 'A' && keySig[1] === 'natural' && keySig[2] === 'major') ||
+        (keySig[0] === 'F' && keySig[1] === 'sharp' && keySig[2] === 'minor')) {
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                    <div className="sharp-2">♯</div>
+                    <div className="sharp-3">♯</div>
+                </div>
+    } else if ( (keySig[0] === 'E' && keySig[1] === 'natural' && keySig[2] === 'major') ||
+        (keySig[0] === 'C' && keySig[1] === 'sharp' && keySig[2] === 'minor')) {
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                    <div className="sharp-2">♯</div>
+                    <div className="sharp-3">♯</div>
+                    <div className="sharp-4">♯</div>
+                </div>
+    } else if ( (keySig[0] === 'B' && keySig[1] === 'natural' && keySig[2] === 'major') ||
+        (keySig[0] === 'G' && keySig[1] === 'sharp' && keySig[2] === 'minor')) {
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                    <div className="sharp-2">♯</div>
+                    <div className="sharp-3">♯</div>
+                    <div className="sharp-4">♯</div>
+                    <div className="sharp-5">♯</div>
+                </div>
+    } else if ( (keySig[0] === 'F' && keySig[1] === 'sharp' && keySig[2] === 'major') ||
+        (keySig[0] === 'D' && keySig[1] === 'sharp' && keySig[2] === 'minor')) {
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                    <div className="sharp-2">♯</div>
+                    <div className="sharp-3">♯</div>
+                    <div className="sharp-4">♯</div>
+                    <div className="sharp-5">♯</div>
+                    <div className="sharp-6">♯</div>
+                </div>
+    } else if ( (keySig[0] === 'C' && keySig[1] === 'sharp' && keySig[2] === 'major') ||
+        (keySig[0] === 'A' && keySig[1] === 'sharp' && keySig[2] === 'minor')) {
+        output = <div className="signature">
+                    <div className="sharp-1">♯</div>
+                    <div className="sharp-2">♯</div>
+                    <div className="sharp-3">♯</div>
+                    <div className="sharp-4">♯</div>
+                    <div className="sharp-5">♯</div>
+                    <div className="sharp-6">♯</div>
+                    <div className="sharp-7">♯</div>
+                </div>
     }
 
     return (
-        <div className="signature">
+        <>
             {output}
-        </div>
+        </>
     )
 }
