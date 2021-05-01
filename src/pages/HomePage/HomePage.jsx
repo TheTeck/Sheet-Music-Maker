2ed312a19a90a6416a07d44d8d8a10ea6e61873f
John Teckemeyer <johnteckemeyer@gmail.com>
2021-04-30 19:31:15 -0700

    Style home page to welcome the user

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/Header/Header';
 import UserNav from '../../components/UserNav/UserNav';
+import { Grid, Header, Divider } from 'semantic-ui-react';
 import "./HomePage.css";
 
 export default function HomePage({ user, handleLogout }) {
@@ -9,7 +10,28 @@ export default function HomePage({ user, handleLogout }) {
             <PageHeader user={user} handleLogout={handleLogout} />
             <div className="body">
                 <UserNav user={user} />
-                This is where I will put info about the app and community works
+                <Grid columns={3}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <div className="welcome-image"></div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div style={{ paddingTop: '20px'}}>
+                                Welcome to
+                                <Header as='h2'>Sheet <span style={{ color: 'red' }}>Music</span> Maker!</Header>
+                                <Divider></Divider>
+                                A completely <span style={{ color: 'red' }}>free</span> sheet music editor I have made as my final project for my full-stack bootcamp. 
+                                <Divider></Divider>
+                                Write your own music and <span style={{ color: 'red' }}>share</span> it with all of your connected friends, or <span style={{ color: 'red' }}>explore</span> what everybody else is creating!
+                                <Divider></Divider>
+                                This app is still in the very early stages of it's existence, so expect vast <span style={{ color: 'red' }}>improvements</span> in the near future!
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className="welcome-image2"></div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         </div>
     )
